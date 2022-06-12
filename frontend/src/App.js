37d6c84db5d8e32273598921f9f41c7162b9aa1a
Arunabh-P <arunabh1995@gmail.com>
2022-06-12 13:24:13 +0100

    login,register page design,implemented routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,27 @@
 import './App.css';
 import Home from './pages/Home';
+import Login from './pages/Login';
 import Product from './pages/Product';
 import ProductDetails from './pages/ProductDetail';
-
+import Register from './pages/Register';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
 function App() {
   return (
     <div className="container">
-      {/* <Home /> */}
-      {/* <Product /> */}
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/products" element={<Product />} />
+          <Route path="/product-detail" element={<ProductDetails />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="login" element={<Login />} />
+        </Routes>
+      </BrowserRouter>
 
-      <ProductDetails/>
     </div>
   );
 }
--- frontend/src/components/Header/index.jsx
+++ frontend/src/components/Header/index.jsx
@@ -2,17 +2,20 @@ import React from 'react'
 import logo from "../../assets/img/KFC-Logo.png"
 import "./header.css"
 import Menu from '../../components/Menu/index'
+import { Link } from "react-router-dom"
 function Header() {
   return (
     <>
       <div className="headerWrapper">
         <div className="logo">
-          <img width="150px" src={logo} alt="" />
+          <Link to="/"><img width="150px" src={logo} alt="" /></Link>
         </div>
         <div className="auth-link">
           <ul>
-            <li><i class="bi bi-geo-alt-fill"></i> Store Locator</li>
-            <li><i class="bi bi-person-fill"></i> Signin/Register</li>
+            <li><Link to=""><i className="bi bi-geo-alt-fill"></i> Store Locator</Link></li>
+            <li><Link to="/login"><i className="bi bi-person-fill"></i> Signin/Register</Link></li>
+            <li><Link to="">My Account</Link></li>
+            <li><Link to="">Logout</Link></li>
           </ul>
         </div>
       </div>
